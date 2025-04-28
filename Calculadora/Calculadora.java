2ace9437885b4de44936d603d7cdcf34520a4004
jhoyosR <jhoyos0410@cue.edu.co>
2025-04-28 16:14:21 -0500

    New: Agrega función de multiplicar

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,3 +1,16 @@
 public class Calculadora {
-    
+     // Atributos
+     int firstNumber;
+     int secondNumber;
+ 
+     // Constructor
+     public Calculadora(int firstNumber, int secondNumber) {
+         this.firstNumber  = firstNumber;
+         this.secondNumber = secondNumber;
+     }
+
+     // Método para multiplicar que recibe dos parámetros de tipo int
+    public void multiplicar (int firstNumber, int secondNumber) {
+        System.out.println("El resultado de la multiplicación entre " + firstNumber + " y " + secondNumber + " es: " + (firstNumber * secondNumber)); 
+    }
 }
