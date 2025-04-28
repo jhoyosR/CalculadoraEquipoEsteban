2825dd643d8d0467d9b59bbc1f1eacb65fafee03
Juliana <Jramirez939@cue.edu.co>
2025-04-28 16:58:44 -0500

    Agrega funcionalidad restar

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,3 +1,13 @@
 public class Calculadora {
-    
+    int firstNumber;
+    int secondNumber;
+
+    public Calculadora(int firstNumber, int secondNumber){
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public void restar(int firstNumber, int secondNumber){
+        System.out.println("La resta entre "+ firstNumber + " y " + secondNumber + " es: " + (firstNumber-secondNumber));
+    }
 }
