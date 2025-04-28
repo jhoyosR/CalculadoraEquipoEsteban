bf947d3be5aed98b8891331e6c682448178a1ecf
jhoyosR <jhoyos0410@cue.edu.co>
2025-04-28 16:23:36 -0500

    New: Agrega función de división

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,3 +1,16 @@
 public class Calculadora {
-    
+    // Atributos
+    int firstNumber;
+    int secondNumber;
+
+    // Constructor
+    public Calculadora(int firstNumber, int secondNumber) {
+        this.firstNumber  = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    // Método para dividir que recibe dos parámetros de tipo int
+    public void dividir (int firstNumber, int secondNumber) {
+        System.out.println("El resultado de la división entre " + firstNumber + " y " + secondNumber + " es: " + (firstNumber / secondNumber)); 
+    }
 }
